44ca99d7700a7bbf7836f76c1020cd12076c418f
iron0120 <60773771+iron0120@users.noreply.github.com>
2023-09-29 12:10:05 +0700

    env

--- silant/silant/settings.py
+++ silant/silant/settings.py
@@ -16,12 +16,19 @@ import dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=%hb95ihvdnbzp!7rdvfk#cdk)yfoig-=4)pmv=#4iu3=04&a3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
